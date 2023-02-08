4019a2063077d8770b85f9467bffebda7645b8b4
lukkam93 <luk.kaminski93@gmail.com>
2023-02-08 07:56:06 +0100

    Dodanie innego sposobu liczenia trafień - pętla for

--- Totolotek.py
+++ Totolotek.py
@@ -5,44 +5,37 @@
 		program zwraca ilość "trafień"
 """
 
-
-#zaszyć generator licz losowych z przedzialu 1-49
-#nie wyswietlaj ich uzytkownikowi
-#petla ktora poprosi o prowadzenie 6 typow
-#mozna wprowadzic tylko liczby z przedzialu 1-49
-#mozna je dodac do listy, a pozniej sprawdzic czy dana liczba znajduje sie w liczbach losowych
-#porownanie dwoch list?
-#wyswietlenie ilosci trafien, nie pokazuje ktore liczby sie trafilo tylko ich ilosc
-
 import random
-lista1 = []
-liczba_losowa = random.sample(range(1,49),6)
+liczba_losowa = random.sample(range(1,50),6)
 
-lista1.append(liczba_losowa)
-print(lista1)
+print(liczba_losowa)
 
 
-lista2 = []
+lista_usera = []
 i = 0
 while i < len(liczba_losowa):
-    strzal = int(input("Podaj swoją liczbę"))
+    strzal = int(input("Podaj swoją liczbę "))
     if strzal <= 0 or strzal > 49:
-        print("Poza zakresem. Podaj liczbę od 1 do 49") #strzały z poza zakresu wciąż traktuje jako podane liczby
+        print("Poza zakresem. Podaj liczbę od 1 do 49")
         continue
-    if strzal not in lista2:
-        lista2.append(strzal)
-        print("To Twój", i+1, "strzał") #jak te dwie linijki przesunąłem maksymalnie w lewo to pętla wciąż pytała o liczbę
+    if strzal not in lista_usera:
+        lista_usera.append(strzal)
+        print("To Twój", i+1, "strzał")
         i += 1
+    else:
+        print("Podałeś już tę wartość")
 
-print("Twoje strzały to", lista2)
+print("Twoje strzały to", lista_usera)
 
-liczba_trafien = [elem for elem in liczba_losowa if elem in lista2]
-print(liczba_trafien)
+#liczenie trafien
+# trafienia = [elem for elem in liczba_losowa if elem in lista2]
+# print(trafienia)
 
-#range 49 nigdy nie wygenruje liczby 49
-#jak zrobie zwykly random to liczby moga sie powtorzyc. jak random.sample to nie.
-#linia 21 z dupy
-#dodaj elsa aby uzytkownik wiedzial ze podal liczbe ktora napisal juz wczesniej
-#inaczej zrobic info o ilosci trafien - uzyj for i in lista2:
-#if i in liczba_losowa
-#append
+
+#liczenie trafien za pomoca petli for
+lista_trafien = []
+for i in lista_usera:
+    if i in liczba_losowa:
+        lista_trafien.append(i)
+ilosc_trafien = len(lista_trafien)
+print(f"Trafiona ilość liczb: {ilosc_trafien}")
